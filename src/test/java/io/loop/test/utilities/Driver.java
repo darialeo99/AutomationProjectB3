2bcc283ed3df5684ec786889d3b02d8e542f385e
leonda <darialeon121999@gmail.com>
2024-07-19 19:01:08 -0400

    first

--- src/test/java/io/loop/test/utilities/Driver.java
+++ src/test/java/io/loop/test/utilities/Driver.java
@@ -2,6 +2,7 @@ package io.loop.test.utilities;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
@@ -36,7 +37,9 @@ public class Driver {
             String browserType = ConfigurationReader.getProperties("browser");
             switch (browserType.toLowerCase()) {
                 case "chrome":
-                    driver = new ChromeDriver();
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--incognito");
+                    driver = new ChromeDriver(options);
                     break;
                 case "firefox":
                     driver = new FirefoxDriver();
